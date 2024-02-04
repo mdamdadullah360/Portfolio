772271d2d7acc0e2e5536f4984f9a7cbb3118a88
Md Amdadullah <97424788+mdamdadullah360@users.noreply.github.com>
2024-02-04 21:55:55 +0600

    Update scroll_nav.js

--- assets/js/scroll_nav.js
+++ assets/js/scroll_nav.js
@@ -1,10 +1,1 @@
-//navigation active start
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if (target.length) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-});
+$('a[href^="#"]').on("click",function(t){var e=$(this.getAttribute("href"));e.length&&(t.preventDefault(),$("html, body").stop().animate({scrollTop:e.offset().top},1e3))});
